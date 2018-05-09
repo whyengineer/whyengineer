5b64391204adedfd209eaaef7ad5c8d253689973
frankie <frankie.zengfu@gmail.com>
2018-05-09 09:40:01 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Nav></Nav>
+    <Slider></Slider>
+    <div class="line"></div>
+    <HC></HC>
+  </div>
+</template>
+
+<script>
+import Nav from './Nav.vue'
+import Slider from './Slider.vue'
+import HC from './HomeC.vue'
+
+export default {
+  components:{
+    Nav,
+    Slider,
+    HC
+  },
+  name: 'app',
+  data () {
+  }
+}
+</script>
+
---
+++ src/Nav.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+        <el-menu
+        :default-active="activeIndex2"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect">
+            <el-menu-item index="1"><img src="https://img.whyengineer.com/logo.png?imageView2/1/w/46/h/46/q/75|imageslim"><span style="font-size:18px"> WhyEngineer</span></el-menu-item>
+            <el-submenu index="2">
+                <template slot="title">硬件</template>
+                <el-menu-item index="2-1" disabled>ESP32-Cricle</el-menu-item>
+                <el-menu-item index="2-2" disabled>ESP32-Snow</el-menu-item>
+                <el-menu-item index="2-3">ESP32-Lin</el-menu-item>
+            </el-submenu>
+            <el-menu-item index="3">软件</el-menu-item>
+            <el-menu-item index="4">维基</el-menu-item>
+            <el-menu-item index="5">Github</el-menu-item>
+        </el-menu>
+  </div>
+</template>
+
+
+
+<script>
+  export default {
+    data() {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1'
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
